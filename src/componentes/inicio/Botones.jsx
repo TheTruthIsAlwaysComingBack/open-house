1a9e2e031631b2a189046263d738ade814abe5b9
Natalia Lozano <acuarionat2004@gmail.com>
2024-05-23 07:18:08 -0400

    creacion-botones

---
+++ src/componentes/inicio/Botones.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Botones.css"
+
+const Botones = () => {
+  return (
+    <div className='botones'>
+        <h2>¡Te invitamos a conocer los proyectos presentados!</h2>
+        <button> <img src={"./src/assets/imagenes/robot-arm.png"} alt="proyectos" /> </button>
+        <hr></hr>
+        <h2>Conoce las opiniones de otras personas</h2>
+        <button> <img src={"./src/assets/imagenes/interview.png"} alt="entrevistas" /> </button>
+        <hr></hr>
+        <h2>Rie un poco con estos memes</h2>
+        <button> <img src={"./src/assets/imagenes/laugh.png"} alt="memes" /> </button>
+    </div>
+  )
+}
+
+export default Botones
--- src/componentes/inicio/Home.jsx
+++ src/componentes/inicio/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Informacion from './Informacion'
+import Botones from './Botones'
 
 const Home = () => {
   return (
     <div>
         <Header/>
         <Informacion/>
+        <Botones/>
     </div>
   )
 }
